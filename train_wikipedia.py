ddf3f82e55bf5ede015d9116147712fdc39ae41c
Dan SASAI <dan.sasai@music.yamaha.com>
2019-12-20 17:25:05 +0900

    [feat] train_wikipedia.py: reflect API modification

--- train_wikipedia.py
+++ train_wikipedia.py
@@ -30,19 +30,20 @@ if __name__ == "__main__":
 
     dic_path = options["dictionary_path"]
     wikipedia_dump_path = options["wikipedia_dump_path"]
-    wikipedia = text_dataset.WikipediaDataset()
-
     output_model_path = options["output_model_path"]
     size = options["size"]
     window = options["window"]
     min_count = options["min_count"]
     epoch = options["epoch"]
     lang = options["lang"]
-    
+
+    if lang == "ja":
+        tokenizer = document_tokenizer.MecabDocumentTokenizer(dic_path)
+    elif lang == "en":
+        tokenizer = document_tokenizer.NltkDocumentTokenizer()
+    wikipedia = text_dataset.WikipediaDataset(tokenizer)
+
     with tempfile.TemporaryDirectory() as temp_dir:
         iter_docs = partial(wikipedia.iter_docs, wikipedia_dump_path, temp_dir)
-        if lang == "ja":
-            tokenizer = document_tokenizer.MecabDocumentTokenizer(dic_path)
-        elif lang == "en":
-            tokenizer = document_tokenizer.NltkDocumentTokenizer()
-        glove_trainer.train_glove_model(output_model_path, iter_docs, tokenizer, size, window, min_count, epoch)
+        
+        glove_trainer.train_glove_model(output_model_path, iter_docs, size, window, min_count, epoch)
